e387ffb47dc9dbc05d4b4fbf0f0d1d5ab3a4ee92
Iuri Sampaio <iurisampaio18@gmail.com>
2020-10-21 21:15:52 -0300

    finalizando o hook useSocket e suas funcionalidades

--- source/services/chat/index.js
+++ source/services/chat/index.js
@@ -4,13 +4,10 @@ io.on('connection', socket => {
     const id = socket.handshake.query.id;
     socket.join(id);
 
-    socket.on( 'envia-msg' , ({ recipients , text })=>{
-        recipients.forEach(recipent => {
-            const newRecipient = recipients.filter(r => r !== recipients );
+    socket.on( 'envia-msg' , ({ recipient , text })=>{
 
-            newRecipient.push(id);
-
-            socket.broadcast.to(recipent).emit( 'recebe-msg' , { recipients: newRecipient, sender: id , text } );
-        })
+        socket.to(recipient.id).emit( 'recebe-msg' , { recipient, sender: id , text } );
+    		
+    	console.log({ recipient, sender: id , text })
     } )
 })
